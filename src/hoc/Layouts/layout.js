70112b698faa8bdece1d3cd99bd937c51984ba99
rahulsinhabarwala <rahulsinhabarwala@gmail.com>
2019-05-29 17:44:46 +0530

    onwheel

--- src/hoc/Layouts/layout.js
+++ src/hoc/Layouts/layout.js
@@ -2,72 +2,84 @@ import React, { Component } from 'react';
 import NavBar from '../../components/Banner/NavBar/navbar';
 import Banner from '../../components/Banner/banner';
 import './layout.scss';
-import throttle from 'lodash.throttle';
-
+import WheelReact from 'wheel-react';
 // import Fotter from '../../Components/Footer/footer';
-
 class Layout extends Component {
 	constructor(props) {
 		super(props);
 		this.handleClick = this.handleClick.bind(this);
-		this.handleWheel = this.handleWheel.bind(this);
-		this.handleClickThrottled = throttle(this.handleWheel, 1000);
 		}
-		componentWillUnmount() {
-			this.handleClickThrottled.cancel();
-		}
-	
+		
 	  handleClick(event) {
 		let getactive = document.querySelectorAll('.navItems a')
 		getactive.forEach( item=>{
 		    item.classList.remove('active');
 		  })
 		  event.target.classList.add('active');
-			switch(event.target.id) {
-				case '0':
-					return this.setState({
-						key:'',
-						value:"Hello,-I'm Justine.-I’m a UI Designer from France."//
-					});
-				case '1':
-					return this.setState({
-						key:'Skills:',
-						value:'Hi,-I’m John,-web developer.'
-					});
-				case '2':
-					return this.setState({
-						key:'About:',
-						value:'Hi,-I’m John,-web developer.'
-					});
-					case '3':
-					return this.setState({
-						key:'MyWork:',
-						value:'Hi,-I’m John,-web developer.'
-					});
-				default:
-					return this.setState({
-						key:'Me:',
-						value:'Hi,-I’m John,-web developer.' 
-				});
-			}
-		
+		  let id =  parseInt(event.target.id,);
+		  this.statechanger(id);		
 	  }
-		handleWheel(event){
-			//console.log(event); // => nullified object.
-			const eventType = event.type;
-  console.log(eventType);
-			//this.props.loadMore();
-			
+	  statechanger(active){
+		console.log(active);
+		switch(active) {
+			case 0:
+				return this.setState({
+					id:0,
+					key:'',
+					value:"Hello,-I'm Justine.-I’m a UI Designer from France."
+				});
+			case 1:
+				return this.setState({
+					id:1,
+					key:'About:',
+					value:'Hi,-I’m John,-web developer.'
+				});
+			case 2:
+				return this.setState({
+					id:2,
+					key:'Skills:',
+					value:'Hi,-I’m John,-web developer.'
+				});
+				case 3:
+				return this.setState({
+					id:3,
+					key:'MyWork:',
+					value:'Hi,-I’m John,-web developer.'
+				});
+			default:
+				return this.setState({
+					id:0,
+					key:'Me:',
+					value:'Hi,-I’m John,-web developer.' 
+			});
 		}
+	  }
 	state={
-		key:'Me :',
-		value:'Hi,-I’m John,-web developer.'
+		id:0,
+		key:'',
+		value:"Hello,-I'm Justine.-I’m a UI Designer from France."
 		}
-	
+	componentDidMount(){
+		WheelReact.config({
+			up: () => {
+			let active = this.state.id+1;
+			this.statechanger(active);	
+			},
+			down: () => {
+				let active
+				if(this.state.id==0){active=3}
+				else{active = this.state.id-1}
+				this.statechanger(active);}
+		  });
+	}
+	componentWillUnmount() {
+		this.handleClickThrottled.cancel();
+		WheelReact.clearTimeout();
+	}
 	render() {
 		return (
-				<div className='container-fluid'
-				onWheel={this.handleClickThrottled}
+				<div className='container-fluid' {...WheelReact.events}
+				//onWheel={this.handleClickThrottled}
 				>
 					<NavBar handleClick={this.handleClick}/>
 					<Banner text={this.state} />
